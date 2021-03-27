910371beacee40a1b8065f2b6fc208c4ff4bc3d2
sameer <sameerpixelbot@gmail.com>
2021-03-27 19:09:18 +0530

    attention fixed

--- attention.py
+++ attention.py
@@ -56,29 +56,30 @@ class MultiHeadAttention(tf.keras.layers.Layer):
     """Split the last dimension into (num_heads, depth).
     Transpose the result such that the shape is (batch_size, num_heads, seq_len, depth)
     """
-    x = tf.reshape(x, (batch_size, -1, self.num_heads, self.depth))
-    return tf.transpose(x, perm=[0, 2, 1, 3])
+    x = tf.reshape(x, (-1, self.num_heads, self.depth))
+    return tf.transpose(x)
 
   def call(self, v, k, q, mask):
-    batch_size = tf.shape(q)[0]
+    #batch_size = tf.shape(q)[0]
+
 
     q = self.wq(q)  # (batch_size, seq_len, d_model)
     k = self.wk(k)  # (batch_size, seq_len, d_model)
     v = self.wv(v)  # (batch_size, seq_len, d_model)
 
-    q = self.split_heads(q, batch_size)  # (batch_size, num_heads, seq_len_q, depth)
-    k = self.split_heads(k, batch_size)  # (batch_size, num_heads, seq_len_k, depth)
-    v = self.split_heads(v, batch_size)  # (batch_size, num_heads, seq_len_v, depth)
+    q = self.split_heads(q, 1)  # (batch_size, num_heads, seq_len_q, depth)
+    k = self.split_heads(k, 1)  # (batch_size, num_heads, seq_len_k, depth)
+    v = self.split_heads(v, 1)  # (batch_size, num_heads, seq_len_v, depth)
 
     # scaled_attention.shape == (batch_size, num_heads, seq_len_q, depth)
     # attention_weights.shape == (batch_size, num_heads, seq_len_q, seq_len_k)
     scaled_attention, attention_weights = scaled_dot_product_attention(
         q, k, v, mask)
 
-    scaled_attention = tf.transpose(scaled_attention, perm=[0, 2, 1, 3])  # (batch_size, seq_len_q, num_heads, depth)
+    scaled_attention = tf.transpose(scaled_attention)  # (batch_size, seq_len_q, num_heads, depth)
 
     concat_attention = tf.reshape(scaled_attention, 
-                                  (batch_size, -1, self.d_model))  # (batch_size, seq_len_q, d_model)
+                                  (-1, self.d_model))  # (batch_size, seq_len_q, d_model)
 
     output = self.dense(concat_attention)  # (batch_size, seq_len_q, d_model)
 
